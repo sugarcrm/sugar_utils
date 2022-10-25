af58442710ef5b9452453a18fb4d2a2d82f01179
Andrew Sullivan Cant <acant@sugarcrm.com>
2022-10-25 14:02:37 -0400

    Add Code Climate coverage reporting to CI
    Using the `paambatti/codeclimate-action` which will automatically download
    and execute the Code Climate test reporter.
    
    * https://github.com/paambaati/codeclimate-action
    * https://github.com/marketplace/actions/code-climate-coverage-action
    * https://github.com/codeclimate/test-reporter
    
    Additionally, I found that the test reporter does not reliably read the
    simplecov JSON format, even though it will attempt to use it by default.
    
    So I have added the lcov formatter, and configured the reporter to use
    it.
    
    * https://github.com/fortissimo1997/simplecov-lcov
    
    Finally, the CC_TEST_REPORTER_ID is stored as a repository secret
    (i.e., `${{ secrets.CC_TEST_REPORTER_ID }}`) and will need to be added
    before this action will work as expected.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,21 @@ require 'pp'
 
 # Setup code coverage
 require 'simplecov'
-SimpleCov.start
+require 'simplecov-lcov'
+require 'simplecov_json_formatter'
+SimpleCov.start do
+  # NOTE: Include the lcov formatter for CodeClimate reporting.
+  # Even though the simplecov JSON format will be output and used by default,
+  # the test reporter does not handle it reliable.
+  # Using the lcov formatter is more reliable.
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::JSONFormatter,
+      SimpleCov::Formatter::LcovFormatter
+    ]
+  )
+end
 
 require 'sugar_utils'
 MultiJson.use(:ok_json)
